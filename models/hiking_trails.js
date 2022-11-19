b34d26576c3b7a031b0f32b632b6464c51a0f9b8
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2022-11-18 20:06:28 -0600

    made a carousel, not working yet, and cards for each trail

---
+++ models/hiking_trails.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const trailsSchema = new mongoose.Schema({
+  trailName: String,
+  image: String,
+  mapsLink: String,
+  description: String,
+  length: Number,
+  difficulty: String,
+  quality: String,
+})
+
+const trailsCollection = mongoose.model('trailInfo', trailsSchema)
+
+module.exports = trailsCollection
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+const app = express()
+app.use(express.urlencoded({extended: true}))
+
+const TrailSchema = require('./models/hiking_trails.js')
+const Data = require('./models/hiking_data.js')
+
+app.use( express.static( "public" ) );
+app.use(methodOverride('_method'))
+
+// TrailSchema.create(Data, (error, createdPokemon) => {
+// //^your schema  //^your pokemon.js
+//   console.log("done!");
+// })
+
+
+// GET Methods
+
+app.get("/", (req, res) => {
+  TrailSchema.find({}, (error, allTrails) => {
+    res.render('trails_index.ejs', {
+      trails: allTrails
+    })
+  })
+
+})
+
+
+
+
+
+app.listen(3000, () => {
+  console.log('listening...');
+})
+
+mongoose.connect('mongodb://localhost:27017/trails', () => {
+  console.log('connection with mongod is established');
+})
